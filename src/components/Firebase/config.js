2dfde972e089406c115ca919a4998880e5511515
Santiago Miqueo <smiqueo@hotmail.com>
2020-05-04 13:23:21 -0500

    Install dependencies and initial setup firebase

---
+++ src/components/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBBpcp4q6AagwHY-FxN_C46CyoeOC52-Ss",
+  authDomain: "gatsby-book-club-4271f.firebaseapp.com",
+  databaseURL: "https://gatsby-book-club-4271f.firebaseio.com",
+  projectId: "gatsby-book-club-4271f",
+  storageBucket: "gatsby-book-club-4271f.appspot.com",
+  messagingSenderId: "67895908618",
+  appId: "1:67895908618:web:280e5d4fdf92c5386d42f3",
+  measurementId: "G-RBWK74Z3Q6",
+}
+
+export default firebaseConfig
